c1c258aa2253e06ce5687cabef200aa076d3db90
Roger Peyer <peyer.roger@gmail.com>
2021-01-21 17:43:24 +0100

    postgres

--- src/main/java/com/rogerpeyer/itemsapi/api/ItemController.java
+++ src/main/java/com/rogerpeyer/itemsapi/api/ItemController.java
@@ -7,6 +7,7 @@ import com.rogerpeyer.itemsapi.persistence.model.ItemPo;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 @AllArgsConstructor
+@Transactional
 class ItemController {
 
   private final ItemConverter itemConverter;
